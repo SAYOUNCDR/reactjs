c355e765caa50874d8c5ccbbbb2ab7859320363a
SAYOUNCDR <pheonixgamer12345@gmail.com>
2025-07-05 19:02:00 +0530

    props

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Greetings } from "./components/Greetings";
 import JSXRules from "./components/JSXRules";
 import { List } from "./components/List";
 import { ProductInfo } from "./components/ProductInfo";
+import { PropsChild } from "./components/PropsChild";
 
 //Components inside a component
 const App = () => {
@@ -13,6 +14,12 @@ const App = () => {
       <ProductInfo />
       <List />
       <Arrayobj />
+
+      {/* Passing Props to child components */}
+      <PropsChild
+        name="Sayoun"
+        gmail="0xSYN.dev@gmail.com"
+      />
     </div>
   );
 };
---
+++ src/components/PropsChild.jsx
@@ -0,0 +1,8 @@
+export const PropsChild = ({ name, gmail }) => {
+  return (
+    <div>
+      <p>{name}</p>
+      <p>{gmail}</p>
+    </div>
+  );
+};
